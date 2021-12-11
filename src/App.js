1afb161309c902fcf9c361d5f726b48a754e707b
Samaheh Loghmani <>
2021-12-10 20:47:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Cards from "./components/Cards";
-import Search from "./components/Search";
-import Logos from "./components/Logos";
+
 import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <Banner />
-                <Cards />
-                <Search />
-                <Logos />
+                <LandingPage />
                 <Footer />
             </div>
         );
