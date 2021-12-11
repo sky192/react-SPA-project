a8f8f53b134c6eba169f9ab3da8c528bf4e56ed2
Samaheh Loghmani <>
2021-12-10 22:43:38 -0500

    add axios and api

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Card from "./Card";
+import styles from "./Products.module.css";
 
 class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get("https://fakestoreapi.com/products")
+            .then((response) => this.setState({ products: response.data }));
+    }
     render() {
         return (
-            <div>
-                <h1>products</h1>
+            <div className={styles.container}>
+                {this.state.products.length ? (
+                    this.state.products.map((product) => (
+                        <Card
+                            key={product.id}
+                            image={product.image}
+                            name={product.title}
+                            cost={`${product.price} $`}
+                        />
+                    ))
+                ) : (
+                    <h1>loading...</h1>
+                )}
             </div>
         );
     }
