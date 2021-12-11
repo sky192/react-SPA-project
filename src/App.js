2e6fb4317142900f246084279daedfe987d1f188
Samaheh Loghmani <>
2021-12-10 21:30:26 -0500

    creared Link for nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
+import Products from "./components/Products";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <LandingPage />
+
+                <Switch>
+                    <Route path="/products" component={Products} />
+                    <Route path="/" component={LandingPage} />
+                </Switch>
+
                 <Footer />
             </div>
         );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import logo from "../images/Logo.png";
 
@@ -8,9 +8,15 @@ const Navbar = () => {
         <header className={styles.header}>
             <div className={styles.listContainer}>
                 <ul className={styles.list}>
-                    <li>Home Page</li>
-                    <li>Products</li>
-                    <li>About Us</li>
+                    <li>
+                        <Link to="/">Home Page</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/aboutus">About Us</Link>
+                    </li>
                 </ul>
             </div>
             <div className={styles.logo}>
